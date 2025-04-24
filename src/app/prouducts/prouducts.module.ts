4f4cb8595f9ad94b35c930d4db0520c94966b15b
ranaelmallah <ranaelmallah88@gmail.com>
2025-04-24 16:01:08 +0200

    last update

---
+++ src/app/prouducts/prouducts.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllprouductsComponent } from './component/allprouducts/allprouducts.component';
+import { ProuductdetailsComponent } from './component/prouductdetails/prouductdetails.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { HttpClientXsrfModule } from '@angular/common/http';
+import { ProductComponent } from './component/product/product.component';
+// import { AppRoutingModule } from  '../app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HomeProductComponent } from './homeProducts/home-product/home-product.component';
+
+
+
+@NgModule({
+  declarations: [
+    AllprouductsComponent,
+    ProuductdetailsComponent,
+    ProductComponent,
+    HomeProductComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    FormsModule,
+    // RouterModule,
+    // AppRoutingModule
+
+ ],exports: [
+  ProductComponent
+  ,AllprouductsComponent,
+  HomeProductComponent
+
+]
+})
+export class ProuductsModule { }
